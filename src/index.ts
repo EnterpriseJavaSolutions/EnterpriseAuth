a83255b1bc71d8cdf43b7327baec51d805132499
errplanedev <110797323+errplanedev@users.noreply.github.com>
2026-01-11 16:46:01 -0800

    More config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = process.env.PORT || 3000; // bun auto handles .env
+
+// express config
+app.use(cookieParser());
+app.use((req, res, next) => {
+  res.header("X-Powered-By", "EnterpriseAuth"); // ;)
+  next();
+});
 
 app.listen(port, () => {
   console.log(`listening on http://0.0.0.0:${port}`);
