2d06cb318ba4aa94e133be29bf6668ffbd64805b
errplanedev <110797323+errplanedev@users.noreply.github.com>
2026-01-13 11:29:42 -0800

    set up DB

---
+++ src/db/schema.ts
@@ -0,0 +1,9 @@
+import { boolean, integer, pgTable, text, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedByDefaultAsIdentity(),
+  username: text().notNull().unique(),
+  admin: boolean().notNull().default(false),
+  hwid: text(), // Not required
+  password: text(),
+});
